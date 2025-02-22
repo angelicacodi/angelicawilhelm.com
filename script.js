e23914d5ab1bf597c084b15293679d991f113881
Angelica Wilhelm <acodi28@gmail.com>
2025-02-22 18:14:25 -0500

    Updated sections

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let noCloseModalOpen = false;
+/**
+ * This function toggles the top left start menu 
+ */
 function toggleMenu() {
     const menu = document.getElementById('start-menu');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
+
+// variable to help with no-close-modal popup
+let noCloseModalOpen = false;
+/**
+ * Toggles the "X" "you can't close this" modal popup
+ */
 function noCloseModalToggle() {
     const noCloseModal = document.getElementById('no-close-modal')
     if (noCloseModalOpen) {
@@ -11,4 +20,13 @@ function noCloseModalToggle() {
         noCloseModal.style.display = "flex";
     }
     noCloseModalOpen = !noCloseModalOpen;
+}
+
+/**
+ * Navigates to internal pages
+ * @param {string} page the page to navigate to (e.g., "/about-me.html", "/projects.html")
+ */
+function gotoPage(page) {
+    const baseUrl = window.location.origin;
+    window.location.href = baseUrl + page;
 }
